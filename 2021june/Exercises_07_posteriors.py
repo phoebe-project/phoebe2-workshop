cbd49c7c8ee525e506476b8bfe2feef809a8e093
Travis CI User <travis@example.org>
2021-06-11 15:01:02 +0000

    TRAVIS: updating ipynb->py sources

---
+++ 2021june/Exercises_07_posteriors.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Exercises #7: Getting Posteriors
+# 
+# Using the results from you [MCMC run](./Exercises_06_mcmc.ipynb), process the results from MCMC into posteriors and parameter uncertainties.
+# 
+# Make sure to check for convergence, set `burnin` and `thin` appropriately, and try propagating to parameters not included in sampling (i.e. masses).
