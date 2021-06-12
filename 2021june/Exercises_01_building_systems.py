7761399381ccf284353bb10a0cf8bb50d98afc82
Travis CI User <travis@example.org>
2021-06-12 16:06:41 +0000

    TRAVIS: updating ipynb->py sources

--- 2021june/Exercises_01_building_systems.py
+++ 2021june/Exercises_01_building_systems.py
@@ -3,13 +3,15 @@
 
 # # Exercises #1: Building Systems
 # 
-# These exercises are designed to be done anytime after Tutorial #2 (constraints)
+# These exercises are designed to be done anytime after Tutorial #2 (constraints).
+# 
+# Don't feel like you need to do all of these - just pick and choose which seem the most interesting to you.  Make sure to save your notebooks and/or bundles, as [tomorrow's exercise](./Exercises_02_forward_models.ipynb) will have you generate synthetic models for these same system(s).
 
-# # \# 1 Sun-Jupiter
+# # \#1: Sun-Jupiter
 # 
 # Create a default binary Bundle, re-parameterize so that you can set both.  Now set the value of the masses and the respective radii such that you have a Jupiter around a Sun.
 # 
-# Hint: try using units to handle the conversion between Jupiter and Solar masses/radii.  You can always do dir(u) to see a list of all available units, or u.solMass.find_equivalent_units().  For other quantities (orbital/rotation periods, sma, etc), you may need to lookup the values since they do not have units/constants available.
+# Hint: try using units to handle the conversion between Jupiter and Solar masses/radii.  You can always do `dir(phoebe.u)` to see a list of all available units, or `phoebe.u.solMass.find_equivalent_units()`.  For other quantities (orbital/rotation periods, sma, etc), you may need to lookup the values since they do not have units/constants available.
 
 # In[ ]:
 
@@ -17,7 +19,7 @@
 
 
 
-# # \# 2 Sun-Earth
+# # \#2: Sun-Earth
 # 
 # Create a Sun-Earth system.  Make sure to get the correct masses, radii, orbital period, separation, eccentricity, and rotational period.
 
@@ -27,7 +29,17 @@
 
 
 
-# # \# 3 Reproduce Binaries from the CALEB Catalog
+# # \#3: Reproduce Systems from the Literature
+# 
+# Find (or pick your favorite) EB solution in the literature and try to map the reported values in the paper into PHOEBE.
+
+# In[ ]:
+
+
+
+
+
+# # \#4: Advanced: Reproduce Binaries from the CALEB Catalog
 # 
 # Pick a system from the [Caleb catalog of EBs](http://caleb.eastern.edu/query_stars_by_type.php), click on one of the "library sets", and then view the BM3 input file.  Try to translate as many of these parameters as you can into PHOEBE.  
 # 
@@ -43,6 +55,8 @@
 # * Caleb's contact -> `phoebe.default_binary()` with semi-detached constraint on both components
 # * Caleb's overcontact -> `phoebe.default_binary(contact_binary=True)`
 # * Caleb's double-contact -> good luck! (you can try semi-detached constraints)
+# 
+# For detached systems, you can try using `R1_BACK`, `R2_BACK` as the equivalent radius, but otherwise you will need to translate the equipotentials `OMEGA_1`, `OMEGA_2` using [phoebe.distortions.roche.pot_to_requiv](http://phoebe-project.org/docs/2.3/api/phoebe.distortions.roche.pot_to_requiv) with `d=1-e` (as potentials are defined at periastron) along with an estimate of the semi-major axis from the RV semi-amplitudes, inclination, and eccentricity.  If you get stuck, don't hesitate to ask for help!
 
 # In[ ]:
 
--- 2021june/Exercises_02_forward_models.py
+++ 2021june/Exercises_02_forward_models.py
@@ -5,9 +5,9 @@
 # 
 # These exercises are designed to be done anytime after Tutorial #5 (plotting)
 
-# # \# 1 System from Exercises #1
+# # \#1: System from Exercises #1
 # 
-# Pick up where you left off from the previous exercises and take the systems you created and build and plot forward models.
+# Pick up where you left off from the [previous exercises](Exercises_01_building_systems.ipynb) and take the systems you created and build and plot forward models.
 
 # In[ ]:
 
@@ -15,9 +15,9 @@
 
 
 
-# # \# 2 Recreate a System from the Literature
+# # \#2: Recreate a System from the Literature
 # 
-# Find a solved EB in the literature and attempt to re-create the model in the paper.
+# Find a solved EB in the literature and attempt to re-create the model in the paper.  Bonus points if you can find one with published data or a synthetic model to compare directly.
 
 # In[ ]:
 
