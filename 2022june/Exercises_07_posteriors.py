e000a4d37c2ee02b3e78991625495999091dd0f0
Kyle <kyleconroy@gmail.com>
2022-06-16 20:14:17 -0400

    clear all previously built files to prepare for renumbering

--- 2022june/Exercises_07_posteriors.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Exercises #7: Getting Posteriors
-# 
-# Using the results from your [MCMC run](./Exercises_06_mcmc.ipynb), process the results from MCMC into posteriors and parameter uncertainties.
-# 
-# Make sure to check for convergence, set `burnin` and `thin` appropriately, and try propagating to parameters not included in sampling (i.e. masses).
