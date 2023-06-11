adb1001d16c732cbc2352efb020267722a7b2844
gh-actions <gh-actions>
2023-06-11 17:28:31 +0000

    building 2023june notebooks
    automatically generated by github build-docs action

--- 2023june/Tutorial_13_optimizers.py
+++ 2023june/Tutorial_13_optimizers.py
@@ -15,7 +15,7 @@
 # 
 # 
 
-# In[1]:
+# In[ ]:
 
 
 import phoebe
@@ -28,7 +28,7 @@ logger = phoebe.logger('error')
 
 # You may need to update the path below to where you saved the bundle from the previous tutorial, or you can download [after_estimators.bundle](https://github.com/phoebe-project/phoebe2-workshop/raw/2023june/data/synthetic/after_estimators.bundle).
 
-# In[2]:
+# In[ ]:
 
 
 #Load Previous Bundle
@@ -37,7 +37,7 @@ b = phoebe.open('data/synthetic/after_estimators.bundle')
 
 # It's useful to remind ourselves what models and compute options we have in the bundle:
 
-# In[3]:
+# In[ ]:
 
 
 print(b.models, b.computes)
@@ -45,7 +45,7 @@ print(b.models, b.computes)
 
 # Now let's take a quick look at our final fit from the previous bundle:
 
-# In[4]:
+# In[ ]:
 
 
 b.run_compute('phoebe01', model='after_estimators', overwrite=True)
@@ -54,7 +54,7 @@ b.plot(model='after_estimators', x='phases', show=True)
 
 # This looks close enough for the optimizer run. We will first initialize a new compute parameter-set and tweak a few optimization options for faster runtime:
 
-# In[5]:
+# In[ ]:
 
 
 b.add_compute(compute='nm_fit',
@@ -65,11 +65,38 @@ b.add_compute(compute='nm_fit',
 
 # To save even more time, we can run the optimizer in phase-space instead of time-space; for that, we need to provide an array of phases in which the model should be computed and optimized:
 
-# In[6]:
+# In[ ]:
 
 
-b.flip_constraint('compute_phases@rv01', solve_for='compute_times@rv01')
-b.set_value_all('compute_phases', dataset='rv01', value=phoebe.linspace(0, 1, 25))
+b.flip_constraint('compute_phases@rv01', solve_for='compute_times')
+b.set_value_all('compute_phases', dataset='rv01', value=phoebe.linspace(0, 1, 26))
+
+
+# In[ ]:
+
+
+b.flip_constraint('compute_phases@lc01', solve_for='compute_times')
+b.set_value_all('compute_phases', dataset='lc01', value=phoebe.linspace(0, 1, 101))
+
+
+# By default, the solver will automatically determine whether to use `compute_times` or `times`:
+
+# In[ ]:
+
+
+print(b['solver_times'])
+
+
+# In[ ]:
+
+
+print(b['solver_times@lc01'])
+
+
+# In[ ]:
+
+
+b.parse_solver_times()
 
 
 # Phoebe has four optimizer methods: 
@@ -83,7 +110,7 @@ b.set_value_all('compute_phases', dataset='rv01', value=phoebe.linspace(0, 1, 25
 # 
 # We start by adding an optimizer and attaching compute options to it:
 
-# In[7]:
+# In[ ]:
 
 
 b.add_solver('optimizer.nelder_mead',  solver='nm_solver', compute='nm_fit')
@@ -91,7 +118,7 @@ b.add_solver('optimizer.nelder_mead',  solver='nm_solver', compute='nm_fit')
 
 # Let's take a look at the parameters:
 
-# In[8]:
+# In[ ]:
 
 
 print(b['nm_solver'])
@@ -99,7 +126,7 @@ print(b['nm_solver'])
 
 # For the tutorial we will reduce the maximum number of iterations to 20; let us also disable light curve data for now, in order to get radial velocity parameters figured out:
 
-# In[9]:
+# In[ ]:
 
 
 b.set_value('maxiter', solver='nm_solver', value=20)
@@ -108,7 +135,7 @@ b.disable_dataset('lc01')
 
 # Now we need to inform the optimizer which parameters should be adjusted:
 
-# In[10]:
+# In[ ]:
 
 
 b['fit_parameters@nm_solver'] = ['vgamma@system', 't0_supconj@binary', 'q@binary', 'asini@binary']
@@ -116,7 +143,7 @@ b['fit_parameters@nm_solver'] = ['vgamma@system', 't0_supconj@binary', 'q@binary
 
 # With everything set, we can now run the solver. This will take a little bit of time because of the non-zero eccentricity but not enough for a coffee run:
 
-# In[11]:
+# In[ ]:
 
 
 b.run_solver('nm_solver', solution='nm_solution', overwrite=True)
@@ -124,7 +151,7 @@ b.run_solver('nm_solver', solution='nm_solution', overwrite=True)
 
 # New (fit) values for adjusted parameters are stored in the `fitted_values` parameter:
 
-# In[12]:
+# In[ ]:
 
 
 print(b['nm_solution'])
@@ -132,7 +159,7 @@ print(b['nm_solution'])
 
 # Alternatively, we can call the `adopt_solution()` method by passing `trial_run=True`:
 
-# In[13]:
+# In[ ]:
 
 
 print(b.adopt_solution('nm_solution', trial_run=True))
@@ -140,7 +167,7 @@ print(b.adopt_solution('nm_solution', trial_run=True))
 
 # We can now run the model with these proposed parameters and inspect the solution visually:
 
-# In[14]:
+# In[ ]:
 
 
 b.run_compute('nm_fit', solution='nm_solution', model='after_nm', overwrite=True)
@@ -151,7 +178,7 @@ b.plot( kind='rv', model='after_nm', x='phases', y='residuals', show=True, legen
 
 # This improves the initial fit, so we can adopt this solution, thus copying proposed solution values to the bundle's face values:
 
-# In[15]:
+# In[ ]:
 
 
 b.adopt_solution('nm_solution')
@@ -159,7 +186,7 @@ b.adopt_solution('nm_solution')
 
 # Now let's return to light curve data; enable them, and disable RVs:
 
-# In[16]:
+# In[ ]:
 
 
 b.disable_dataset('rv01')
@@ -168,7 +195,7 @@ b.enable_dataset('lc01')
 
 # Depending on the number of data points, computing the forward model can take a long time. That means that _optimizing_ the model can take a _very_ long time. When light curve data do not exhibit significant out-of-eclipse variability, we can limit the optimizer to eclipse regions, thus saving us appreciable time. We will use our previous `lcgeom_solution` to mask out everything else. Eclipse regions are stored in the `eclipse_edges` parameter:
 
-# In[17]:
+# In[ ]:
 
 
 print(b['eclipse_edges@lcgeom_solution'])
@@ -176,7 +203,7 @@ print(b['eclipse_edges@lcgeom_solution'])
 
 # The masking parameter is associated with the datasets:
 
-# In[18]:
+# In[ ]:
 
 
 print(b['mask_phases'])
@@ -184,7 +211,7 @@ print(b['mask_phases'])
 
 # To automatically populate these arrays, we will set `mask_phases` as a parameter to be adopted from the `lcgeom_solution` and then adopt the solution:
 
-# In[19]:
+# In[ ]:
 
 
 b.set_value(solution = 'lcgeom_solution', qualifier='adopt_parameters', value=['mask_phases'])
@@ -193,7 +220,7 @@ b.adopt_solution('lcgeom_solution')
 
 # Now the masked phases are populated by taking `eclipse_edges` and padding 30% of the eclipse width to ascertain adequate eclipse coverage:
 
-# In[20]:
+# In[ ]:
 
 
 print(b['mask_phases'])
@@ -201,7 +228,7 @@ print(b['mask_phases'])
 
 # The easiest way to see this in action is to visualize it:
 
-# In[21]:
+# In[ ]:
 
 
 b.plot(kind='lc', model='after_estimators', x='phases', show='True')
@@ -209,7 +236,7 @@ b.plot(kind='lc', model='after_estimators', x='phases', show='True')
 
 # Recall that, for estimators, we used $R_2/R_1$ and $R_1+R_2$ to parametrize the model. As we will run the actual forward model, it serves our purpose better to use $R_1$ and $R_2$ as independent parameters. Let us flip the constraint and mark relevant parameters for adjustment:
 
-# In[22]:
+# In[ ]:
 
 
 b.flip_constraint('requiv@primary', solve_for='requivsumfrac@binary')
@@ -221,7 +248,7 @@ b['fit_parameters'] = ['teffratio@binary',
 
 # As we set all solver parameters already, we can now simply run it:
 
-# In[23]:
+# In[ ]:
 
 
 b.run_solver('nm_solver', solution='nm_solution', overwrite=True)
@@ -229,7 +256,7 @@ b.run_solver('nm_solver', solution='nm_solution', overwrite=True)
 
 # As before, we can inspect the adjusted values either by looking at `fitted_values` or by running `adopt_solution()` with `trial_run=True`:
 
-# In[24]:
+# In[ ]:
 
 
 print(b.adopt_solution('nm_solution', trial_run=True))
@@ -237,7 +264,7 @@ print(b.adopt_solution('nm_solution', trial_run=True))
 
 # What did that do to our light curve fit?
 
-# In[25]:
+# In[ ]:
 
 
 b.run_compute('nm_fit', solution='nm_solution', model='after_nmlc')
@@ -248,7 +275,7 @@ b.plot(kind='lc', model='after_nmlc', x='phases', y='residuals', show=True, lege
 
 # As before, this looks reasonable, so we can adopt the solution:
 
-# In[26]:
+# In[ ]:
 
 
 b.adopt_solution('nm_solution')
@@ -256,7 +283,7 @@ b.adopt_solution('nm_solution')
 
 # We can take a closer look at the primary eclipse:
 
-# In[27]:
+# In[ ]:
 
 
 b.plot(kind='lc', x='phases', model='after_nmlc', xlim=[-0.2,0.2], show=True, legend=True, marker = 'o')
